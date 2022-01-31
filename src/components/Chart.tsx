bb31d3f5ea3c73300dd44f7053f8d426c6bb8ca6
sean-b765 <61043071+sean-b765@users.noreply.github.com>
2022-01-31 12:01:00 +0800

    Add time picker for charts

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,6 +1,6 @@
 import { motion } from 'framer-motion'
 import moment from 'moment'
-import React from 'react'
+import React, { useState } from 'react'
 import {
 	AreaChart,
 	Area,
@@ -13,18 +13,27 @@ import {
 import { useAppSelector } from '../app/hooks'
 
 const Chart = ({ selectedLocation }) => {
+	const [time, setTime] = useState(999)
 	const _data = useAppSelector(
 		(state) => state.modal.historicalList[selectedLocation]
 	)
 
 	const max = useAppSelector((state) => state.modal.selectedCountry.cases)
-	const leftMargin = Number(max) >= 10_000_000 ? 50 : 30
+	const leftMargin = Number(max) >= 10_000_000 ? 40 : 20
 
-	const data = _data?.map((datum) => {
-		return Number(datum.new_cases) < 0
-			? { ...datum, xAxis: moment(datum.date).format('MMM YY'), new_cases: '0' }
-			: { ...datum, xAxis: moment(datum.date).format('MMM YY') }
-	})
+	const data = _data
+		?.map((datum) => {
+			return Number(datum.new_cases) < 0
+				? {
+						...datum,
+						xAxis: moment(datum.date).format('D MMM YY'),
+						new_cases: '0',
+				  }
+				: { ...datum, xAxis: moment(datum.date).format('D MMM YY') }
+		})
+		.filter((datum) =>
+			moment(datum.date).isAfter(moment().subtract(time, 'months'))
+		)
 
 	const pending = useAppSelector((state) => state.modal.pending)
 
@@ -96,83 +105,113 @@ const Chart = ({ selectedLocation }) => {
 			</span>
 		</div>
 	) : data?.length ? (
-		<motion.div
-			initial={{ opacity: 0 }}
-			exit={{ opacity: 0 }}
-			animate={{ opacity: 1 }}
-			transition={{ duration: 0.4 }}
-			className="charts"
-		>
-			<ResponsiveContainer>
-				<AreaChart
-					width={500}
-					height={400}
-					data={data}
-					margin={{
-						top: 10,
-						right: 0,
-						left: leftMargin,
-						bottom: 100,
-					}}
-				>
-					<CartesianGrid strokeDasharray="3 3" />
-					<XAxis dataKey="xAxis" angle={90} dy={40} dx={40} />
-					<YAxis tickFormatter={(tick) => tick.toLocaleString()} />
-					<Tooltip />
-					<Area
-						type="monotone"
-						dataKey="total_cases"
-						name="Total cases"
-						stackId="1"
-						stroke="#8884d8"
-						fill="#8884d868"
-					/>
-					<Area
-						type="monotone"
-						dataKey="total_deaths"
-						name="Total deaths"
-						stackId="1"
-						stroke="#82ca9d"
-						fill="#82ca9e6e"
-					/>
-				</AreaChart>
-			</ResponsiveContainer>
+		<>
+			<select>
+				<option onClick={() => setTime(999)}>All time</option>
+				<option onClick={() => setTime(24)}>2 years</option>
+				<option onClick={() => setTime(12)}>1 year</option>
+				<option onClick={() => setTime(6)}>6 months</option>
+				<option onClick={() => setTime(3)}>3 months</option>
+				<option onClick={() => setTime(1)}>1 month</option>
+			</select>
+			<motion.div
+				initial={{ opacity: 0 }}
+				exit={{ opacity: 0 }}
+				animate={{ opacity: 1 }}
+				transition={{ duration: 0.4 }}
+				className="charts"
+			>
+				<ResponsiveContainer>
+					<AreaChart
+						width={500}
+						height={400}
+						data={data}
+						margin={{
+							top: 10,
+							right: 10,
+							left: leftMargin,
+							bottom: 100,
+						}}
+					>
+						<CartesianGrid strokeDasharray="3 3" />
+						<XAxis
+							dataKey="xAxis"
+							label="Total Cases"
+							angle={90}
+							dy={50}
+							dx={0}
+							fontSize={'0.8rem'}
+						/>
+						<YAxis
+							tickFormatter={(tick) => tick.toLocaleString()}
+							fontSize={'0.8rem'}
+						/>
+						<Tooltip />
+						<Area
+							type="monotone"
+							dataKey="total_cases"
+							name="Total cases"
+							stackId="1"
+							stroke="#8884d8"
+							fill="#8884d868"
+						/>
+						<Area
+							type="monotone"
+							dataKey="total_deaths"
+							name="Total deaths"
+							stackId="1"
+							stroke="#82ca9d"
+							fill="#82ca9e6e"
+						/>
+					</AreaChart>
+				</ResponsiveContainer>
 
-			<ResponsiveContainer>
-				<AreaChart
-					width={500}
-					height={400}
-					data={data}
-					margin={{
-						top: 10,
-						right: 0,
-						left: leftMargin,
-						bottom: 100,
-					}}
-				>
-					<CartesianGrid strokeDasharray="3 3" />
-					<XAxis dataKey="xAxis" angle={90} dy={40} dx={40} />
-					<YAxis tickFormatter={(tick) => tick.toLocaleString()} />
-					<Tooltip />
-					<Area
-						type="monotone"
-						dataKey="new_cases"
-						name="New cases"
-						stackId="1"
-						stroke="#8884d8"
-						fill="#8884d868"
-					/>
-					<Area
-						type="monotone"
-						dataKey="new_deaths"
-						name="New deaths"
-						stackId="1"
-						stroke="#82ca9d"
-						fill="#82ca9e6e"
-					/>
-				</AreaChart>
-			</ResponsiveContainer>
-		</motion.div>
+				<ResponsiveContainer>
+					<AreaChart
+						width={500}
+						height={400}
+						data={data}
+						margin={{
+							top: 10,
+							right: 10,
+							left: leftMargin,
+							bottom: 100,
+						}}
+					>
+						<CartesianGrid strokeDasharray="3 3" />
+						<XAxis
+							dataKey="xAxis"
+							fontSize={'0.8rem'}
+							angle={90}
+							dy={50}
+							dx={0}
+							label="New Cases"
+						/>
+						<YAxis
+							tickFormatter={(tick) => tick.toLocaleString()}
+							fontSize={'0.8rem'}
+						/>
+						<Tooltip />
+						<Area
+							type="monotone"
+							dataKey="new_cases"
+							name="New cases"
+							stackId="1"
+							stroke="#8884d8"
+							fill="#8884d868"
+						/>
+						<Area
+							type="monotone"
+							dataKey="new_deaths"
+							name="New deaths"
+							stackId="1"
+							stroke="#82ca9d"
+							fill="#82ca9e6e"
+						/>
+					</AreaChart>
+				</ResponsiveContainer>
+			</motion.div>
+		</>
 	) : (
 		<div>No historical data.</div>
 	)
